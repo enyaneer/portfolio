ce1da1f8ac4701190b0b88c7bc068292ead5a9e1
Enya <enya.baez@gmail.com>
2020-01-31 18:55:18 -0500

    added react components

---
+++ client/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Routes from './routes'
+import Navbar from './components/navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes />
+    </div>
+  )
+}
+
+export default App
--- client/index.js
+++ client/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router } from 'react-router-dom'
+import App from './app'
+import history from './history'
 
 ReactDOM.render(
-  <div>If this shows up jump for joy</div>,
+  <Router history={history}>
+    <App />
+  </Router>,
   document.getElementById('app')
 )
