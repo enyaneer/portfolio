68afb878854bcf5b8a405a82dfdc3f147584d2bb
Enya <enya.baez@gmail.com>
2020-01-31 19:47:43 -0500

    embedded yt vid on landing

--- client/components/landing.js
+++ client/components/landing.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Landing = () => {
-  return <div>Landing Page hello!</div>
+class Landing extends Component {
+  constructor() {
+    super()
+    this.state = { videoUrl: 'https://www.youtube.com/embed/lRTtMcx6rSM' }
+  }
+  render() {
+    return (
+      <div>
+        Landing Page hello!
+        <div
+          style={{
+            position: 'relative',
+            paddingBottom: '56.25%' /* 16:9 */,
+            paddingTop: 25,
+            height: 0,
+          }}
+        >
+          <iframe
+            id='background-vid'
+            loop
+            autoPlay
+            style={{
+              position: 'absolute',
+              width: '100%',
+              height: '100%',
+              left: 0,
+              top: 0,
+            }}
+            src={this.state.videoUrl}
+            frameBorder='0'
+          />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Landing
