060b0a6529fe2317bc8d73c5e498481341209ba2
Enya <enya.baez@gmail.com>
2020-02-22 18:13:48 -0500

    added icon links

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -12,11 +12,21 @@ const Navbar = () => {
       </div>
       <div id='social'>
         social media
-        <ul>
-          <li>twitter</li>
-          <li>github</li>
-          <li>linkedin</li>
-        </ul>
+        <div>
+          <a href='https://www.linkedin.com/in/enya-bf/'>
+            <ion-icon size='large' name='logo-linkedin'></ion-icon>
+          </a>
+        </div>
+        <div>
+          <a href='https://github.com/Afrogrrrl'>
+            <ion-icon size='large' name='logo-github'></ion-icon>
+          </a>
+        </div>
+        <div>
+          <a href='https://twitter.com/latechnica1'>
+            <ion-icon size='large' name='logo-twitter'></ion-icon>
+          </a>
+        </div>
       </div>
     </div>
   )
